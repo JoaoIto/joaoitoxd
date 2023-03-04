e8b1be015c6f1c00a09c3a9abf840137384e1bc9
João <joaovictorpfr@gmail.com>
2023-03-04 18:45:26 -0300

    Add fix errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import React, { useRef, useEffect } from 'react';
-import { styles } from './index.css';
-import Typed from 'typed.js';
+import React, { useRef, useEffect } from "react";
+import { styles } from "./index.css";
+import Typed from "typed.js";
 
 interface HeaderProps {
   title: string;
 }
 
-interface MyTypedOptions extends Typed.TypedOptions {
+interface MyTypedOptions{
   strings: string[];
   typeSpeed: number;
   backSpeed: number;
@@ -17,7 +17,7 @@ export function Header(props: HeaderProps) {
   const { title } = props;
   const titleRef = useRef<HTMLHeadingElement>(null);
 
-    useEffect(() => {
+  useEffect(() => {
     if (titleRef.current) {
       const options: MyTypedOptions = {
         strings: [title],
@@ -32,13 +32,15 @@ export function Header(props: HeaderProps) {
     }
   }, [title]);
 
-
-
   return (
     <header className={styles.header}>
       <h3 className={styles.subtitle}>Front-End</h3>
-      <h1 ref={titleRef} className={styles.title} ></h1>
-      <img height="200" src="https://raw.githubusercontent.com/JoaoIto/joaoitoxd/00953735853a57bf59adc7db1bace7d94fc0067c/src/assets/devWeb.svg" alt="" />
+      <h1 ref={titleRef} className={styles.title}></h1>
+      <img
+        height="200"
+        src="https://raw.githubusercontent.com/JoaoIto/joaoitoxd/00953735853a57bf59adc7db1bace7d94fc0067c/src/assets/devWeb.svg"
+        alt=""
+      />
     </header>
   );
-};
+}
