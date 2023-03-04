875ab995d3418a3a3657155da102d0d098c5175b
João <joaovictorpfr@gmail.com>
2023-03-04 17:13:15 -0300

    Base listLinks

--- src/components/Link/Links.tsx
+++ src/components/Link/Links.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Card } from './index';
+import { links } from './index.css';
 
 export const LinkList: React.FC = () => {
   return (
-    <div>
-      <h1>Links úteis</h1>
-      <ul>
-        <li>
-          <Card url="https://www.google.com" title="Google" />
-        </li>
-        <li>
-          <Card url="https://www.facebook.com" title="Facebook" />
-        </li>
-        <li>
-          <Card url="https://www.github.com" title="GitHub" />
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={links}>
+          <Card url="https://www.linkedin.com/in/jo%C3%A3o-victor-p%C3%B3voa-fran%C3%A7a-97502420b/" color="#0e76a8">
+            Linkedin
+          </Card>
+
+          <Card url="https://github.com/JoaoIto" color="#171515">
+            GitHub
+          </Card>
+
+          <Card url="https://www.instagram.com/joaoitoxd/" color="#87536a">
+            Instagram
+          </Card>
+
+          <Card url="mailto:joaovictorpfr@gmail.com?subject=Assunto" color="#cacaca">
+            Email
+          </Card>
+      </div>
+    </>
   );
 };
--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React from "react";
 
-type LinkProps = {
+type ButtonLinkProps = {
   url: string;
-  title: string;
-}
+  color?: string;
+  children?: React.ReactNode;
+};
 
-export const Card: React.FC<LinkProps> = ({ url, title }) => {
+export const Card: React.FC<ButtonLinkProps> = ({ url, color = "blue", children }) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
-      {title}
+    <a href={url}>
+      <div style={{ 
+        backgroundColor: color, 
+        display: "flex",
+        alignItems: "center",
+        justifyContent: 'center',
+        width: "80%", 
+        padding: "10px 20px",
+        margin: "25px 10px", 
+        border: '1px solid #fafafa',
+        borderRadius: 10,
+        color: "#fff", 
+        textDecoration: "none",
+        fontFamily: 'Inter, sans-serif',
+        fontSize: '1.5rem' }}>
+      {children}
+      </div>
     </a>
   );
 };
