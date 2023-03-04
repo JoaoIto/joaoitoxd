b28854d1720ea3df6c331fa3be8ad0038a985d13
João <joaovictorpfr@gmail.com>
2023-03-04 20:29:00 -0300

    icons link webpages

--- src/components/Link/Links.tsx
+++ src/components/Link/Links.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import { Card } from './index';
+import React from "react";
+import { Card } from "./index";
+
+const iconUrls = {
+  github:
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+  gmail:
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg",
+  instagram:
+    "https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/instagram.svg",
+  linkedin:
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg",
+};
 
 export const LinkList: React.FC = () => {
   return (
     <>
       <div>
-          <Card url="https://www.linkedin.com/in/jo%C3%A3o-victor-p%C3%B3voa-fran%C3%A7a-97502420b/" color="#0e76a8">
-            Linkedin
-          </Card>
+        <Card
+          url="https://www.linkedin.com/in/jo%C3%A3o-victor-p%C3%B3voa-fran%C3%A7a-97502420b/"
+          color="#0e76a8"
+        >
+          <img height="40" src={iconUrls.linkedin} alt="Icon de linkedin" />
+          Linkedin
+        </Card>
 
-          <Card url="https://github.com/JoaoIto" color="#171515">
-            GitHub
-          </Card>
+        <Card url="https://github.com/JoaoIto" color="#818181">
+          <img height="40" src={iconUrls.github} alt="Icon do GitHub" />
+          GitHub
+        </Card>
 
-          <Card url="https://www.instagram.com/joaoitoxd/" color="#87536a">
-            Instagram
-          </Card>
+        <Card url="https://www.instagram.com/joaoitoxd/" color="#87536a">
+          <img height="40" src={iconUrls.instagram} alt="Icone do instagram" />
+          Instagram
+        </Card>
 
-          <Card url="mailto:joaovictorpfr@gmail.com?subject=Assunto" color="#8991ff">
-            Email
-          </Card>
+        <Card
+          url="mailto:joaovictorpfr@gmail.com?subject=Assunto"
+          color="#8991ff"
+        >
+          <img height="40" src={iconUrls.gmail} alt="Icon de email" />
+          Email
+        </Card>
       </div>
     </>
   );
--- src/components/Link/index.css.ts
+++ src/components/Link/index.css.ts
@@ -1,19 +1,19 @@
-import { style } from '@vanilla-extract/css';
+import { style } from "@vanilla-extract/css";
 
 export const linkNo = style({
-    textDecoration: "none",
+  textDecoration: "none",
 });
 
 export const links = style({
-        display: "flex",
-        alignItems: "center",
-        justifyContent: 'center',
-        width: "80%", 
-        padding: "20px",
-        margin: "25px 10px", 
-        border: '1px solid #fafafa',
-        borderRadius: 10,
-        color: "#fff",
-        fontFamily: 'Inter, sans-serif',
-        fontSize: '1.5rem' 
-})
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-evenly",
+  width: "80%",
+  padding: "20px",
+  margin: "25px 10px",
+  border: "1px solid #fafafa",
+  borderRadius: 10,
+  color: "#fff",
+  fontFamily: "Inter, sans-serif",
+  fontSize: "1.5rem",
+});
