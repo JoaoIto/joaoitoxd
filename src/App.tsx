0295c15b67c2910f66857cfdfad225c6e4b80712
JoaoIto <joaovictorpfr@gmail.com>
2023-09-13 09:46:09 -0300

    contats buttons and style

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,46 @@ import {ModeToggle} from "@/components/mode-toggle.tsx";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import {Terminal} from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {Button} from "@/components/ui/button.tsx";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 function App() {
+    const openSocialMedia = (url: string) => {
+        window.location.href = url;
+    };
     return (
-        <div>
-            <div className="w-screen h-80 flex-col items-center justify-around text-center">
+            <div className="w-screen h-screen flex-col items-center justify-around text-center">
                 <div className="flex flex-col items-center justify-center text-center">
                     <div className="flex items-center">
                     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                     <ModeToggle/>
                 </ThemeProvider>
                         <div className="flex items-center">
-                <h1 className="text-2xl p-4 font-bold underline">JoãoItoxd</h1>
+                            <HoverCard>
+                                <HoverCardTrigger className="font-bold p-2 text-xl">@joaoitoxd</HoverCardTrigger>
+                                <HoverCardContent>
+                                    Software Web Enginner - Programador Web :)
+                                </HoverCardContent>
+                            </HoverCard>
+
                             <Avatar className="border-2 border-solid border-slate-300">
-                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarImage src="https://avatars.githubusercontent.com/u/78181193?v=4" />
                                 <AvatarFallback>CN</AvatarFallback>
                             </Avatar>
                         </div>
@@ -30,8 +55,89 @@ function App() {
                     </AlertDescription>
                 </Alert>
                 </div>
+
+                <div className="flex flex-col items-center">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger onClick={() => openSocialMedia('https://www.instagram.com/joaoitoxd')} className="w-60 bg-purple-600 p-4 m-2 rounded border border-solid border-purple-200">Instagram</TooltipTrigger>
+                        <TooltipContent>
+                            <p>Visite agora!</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger onClick={() => openSocialMedia('https://github.com/joaoito')} className="w-60 bg-slate-900 p-4 m-2 rounded border border-solid border-slate-400">GitHub</TooltipTrigger>
+                        <TooltipContent>
+                            <p>projetinhos!</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger onClick={() => openSocialMedia('https://www.linkedin.com/in/jo%C3%A3o-victor-p%C3%B3voa-fran%C3%A7a-97502420b/')} className="w-60 bg-blue-500 p-4 m-2 rounded border border-solid border-blue-100">Linkedin</TooltipTrigger>
+                        <TooltipContent>
+                            <p>Trabalho e mais trabalho...</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger onClick={() => openSocialMedia('mailto:joaovictorpfr@gmail.com')} className="w-60 bg-red-600 p-4 m-2 rounded border border-solid border-red-300">Gmail</TooltipTrigger>
+                            <TooltipContent>
+                                <p>Manda lá!</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                    <Accordion className="flex w-60 items-center justify-center" type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Mas o que o joão faz?</AccordionTrigger>
+                            <AccordionContent>
+                                Olá! Bom, eu construo qualquer coisa com software, mais sou especializado
+                                em criação de sites e serviços para a internet, web!
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <Accordion className="flex w-60  items-center justify-center" type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Você é programador a quanto tempo?</AccordionTrigger>
+                            <AccordionContent>
+                                Eu estudo programação desde 2020, quando na pandemia decidi focar diretamente na área,
+                                mas programação se tornou trabalho de fato a partir de 2021 quando já estava pegando
+                                serviços freelance, e depois na segunda metade de 2022 já estava empregado como Engenheiro
+                                de Software Front-End.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <Accordion className="flex w-60  items-center justify-center" type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Por quê escolheu essa área?</AccordionTrigger>
+                            <AccordionContent>
+                                Sempre amei e fui entusiasta da tecnologia, só decidi tornar isso profissional
+                                quando vi que aquilo que eu gostava podia me retornar de alguma forma...
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <Accordion className="flex w-60 items-center justify-center" type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Precisava de um serviço de Marketing para minha loja, posso te contactar?</AccordionTrigger>
+                            <AccordionContent>
+                                Siiimm!! Se pretende que eu faça um serviço e confia no meu trabalho pode me contatar! Se prefere uma proposta
+                                profissional e formal, meu email está linkado nos botões acima.
+
+                                Se caso prefira um contato direto, meu instagram é aberto para direct.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
             </div>
-        </div>
     )
 }
 
