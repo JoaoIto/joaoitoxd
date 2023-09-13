a53f61672fd1fe286084a08e2184395a8b16d062
JoaoIto <joaovictorpfr@gmail.com>
2023-09-13 08:49:24 -0300

    base avatar, title, and spaces style

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,34 @@ import { ThemeProvider } from "@/components/theme-provider"
 import {ModeToggle} from "@/components/mode-toggle.tsx";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import {Terminal} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
     return (
         <div>
-            <div className="w-screen h-80 flex-col items-center justify-around">
-                <div className="flex">
-                <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <div className="w-screen h-80 flex-col items-center justify-around text-center">
+                <div className="flex flex-col items-center justify-center text-center">
+                    <div className="flex items-center">
+                    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                     <ModeToggle/>
                 </ThemeProvider>
-                <h1 className="text-2xl p-4 font-bold">JoãoItoxd</h1>
-                </div>
-                    <Alert>
+                        <div className="flex items-center">
+                <h1 className="text-2xl p-4 font-bold underline">JoãoItoxd</h1>
+                            <Avatar className="border-2 border-solid border-slate-300">
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </div>
+                    </div>
+                        <Alert className="w-60">
                     <Terminal className="h-4 w-4" />
-                    <AlertTitle>Heads up!</AlertTitle>
+                    <AlertTitle>Explore!</AlertTitle>
                     <AlertDescription>
-                        You can add components and dependencies to your app using the cli.
+                        Esse é meu site pessoal de links! Bonitin né?
                     </AlertDescription>
                 </Alert>
+                </div>
             </div>
         </div>
     )
